6f1cb46aea55c4dcad6ff8d0ca2582477b3a7f3a
Jakob Borg <jakob@kastelo.net>
2023-01-27 09:57:06 +0100

    Redirect to repo root instead of go doc

--- main.go
+++ main.go
@@ -4,18 +4,18 @@
 
 // Go-import-redirector is an HTTP server for a custom Go import domain.
 // It responds to requests in a given import path root with a meta tag
-// specifying the source repository for the ``go get'' command and an
-// HTML redirect to the godoc.org documentation page for that package.
+// specifying the source repository for the “go get” command and an
+// HTML redirect to the source repo for that package.
 //
 // Usage:
 //
 //	go-import-redirector [-addr address] [-tls] [-vcs sys] <import> <repo>
 //
-// Go-import-redirector listens on address (default ``:80'')
+// Go-import-redirector listens on address (default “:80”)
 // and responds to requests for URLs in the given import path root
-// with one meta tag specifying the given source repository for ``go get''
+// with one meta tag specifying the given source repository for “go get”
 // and another meta tag causing a redirect to the corresponding
-// godoc.org documentation page.
+// source repo page.
 //
 // For example, if invoked as:
 //
@@ -24,7 +24,7 @@
 // then the response for 9fans.net/go/acme/editinacme will include these tags:
 //
 //	<meta name="go-import" content="9fans.net/go git https://github.com/9fans/go">
-//	<meta http-equiv="refresh" content="0; url=https://godoc.org/9fans.net/go/acme/editinacme">
+//	<meta http-equiv="refresh" content="0; url=https://github.com/9fans/go">
 //
 // If both <import> and <repo> end in /*, the corresponding path element
 // is taken from the import path and substituted in repo on each request.
@@ -35,11 +35,11 @@
 // then the response for rsc.io/x86/x86asm will include these tags:
 //
 //	<meta name="go-import" content="rsc.io/x86 git https://github.com/rsc/x86">
-//	<meta http-equiv="refresh" content="0; url=https://godoc.org/rsc.io/x86/x86asm">
+//	<meta http-equiv="refresh" content="0; url=https://github.com/rsc/x86">
 //
 // Note that the wildcard element (x86) has been included in the Git repo path.
 //
-// The -addr option specifies the HTTP address to serve (default ``:http'').
+// The -addr option specifies the HTTP address to serve (default “:http”).
 //
 // The -tls option causes go-import-redirector to serve HTTPS on port 443,
 // loading an X.509 certificate and key pair from files in the current directory
@@ -48,15 +48,14 @@
 // Like for http.ListenAndServeTLS, the certificate file should contain the
 // concatenation of the server's certificate and the signing certificate authority's certificate.
 //
-// The -vcs option specifies the version control system, git, hg, or svn (default ``git'').
+// The -vcs option specifies the version control system, git, hg, or svn (default “git”).
 //
-// Deployment on Google Cloud Platform
+// # Deployment on Google Cloud Platform
 //
 // For the case of a redirector for an entire domain (such as rsc.io above),
 // the Makefile in this directory contains recipes to deploy a trivial VM running
 // just this program, using a static IP address that can be loaded into the
 // DNS configuration for the target domain.
-//
 package main
 
 import (
@@ -73,7 +72,6 @@ import (
 var (
 	addr       = flag.String("addr", ":http", "serve http on `address`")
 	vcs        = flag.String("vcs", "git", "set version control `system`")
-	godocURL   = flag.String("godoc-url", "", "URL to send the browser to if not fetched using go get")
 	importPath string
 	repoPath   string
 	wildcard   int
@@ -110,8 +108,6 @@ func main() {
 		repoPath = strings.TrimSuffix(repoPath, "/*")
 	}
 
-	*godocURL = strings.TrimRight(*godocURL, "/")
-
 	http.HandleFunc(strings.TrimSuffix(importPath, "/")+"/", redirect)
 	http.HandleFunc(importPath+"/.ping", pong) // non-redirecting URL for debugging TLS certificates
 	err := http.ListenAndServe(*addr, nil)
@@ -125,10 +121,10 @@ var tmpl = template.Must(template.New("main").Parse(`<!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 <meta name="go-import" content="{{.ImportRoot}} {{.VCS}} {{.VCSRoot}}">
-<meta http-equiv="refresh" content="0; url={{.GoDocURL}}/{{.ImportRoot}}{{.Suffix}}">
+<meta http-equiv="refresh" content="0; url={{.VCSRoot}}">
 </head>
 <body>
-Redirecting to docs at <a href="{{.GoDocURL}}/{{.ImportRoot}}{{.Suffix}}">{{.GoDocURL}}/{{.ImportRoot}}{{.Suffix}}</a>...
+Redirecting to <a href="{{.VCSRoot}}">{{.VCSRoot}}</a>...
 </body>
 </html>
 `))
@@ -138,7 +134,6 @@ type data struct {
 	VCS        string
 	VCSRoot    string
 	Suffix     string
-	GoDocURL   string
 }
 
 func redirect(w http.ResponseWriter, req *http.Request) {
@@ -146,7 +141,7 @@ func redirect(w http.ResponseWriter, req *http.Request) {
 	var importRoot, repoRoot, suffix string
 	if wildcard > 0 {
 		if path == importPath {
-			http.Redirect(w, req, *godocURL+"/"+importPath, http.StatusFound)
+			http.Redirect(w, req, repoPath, http.StatusFound)
 			return
 		}
 		if !strings.HasPrefix(path, importPath+"/") {
@@ -180,7 +175,6 @@ func redirect(w http.ResponseWriter, req *http.Request) {
 		VCS:        *vcs,
 		VCSRoot:    repoRoot,
 		Suffix:     suffix,
-		GoDocURL:   *godocURL,
 	}
 	var buf bytes.Buffer
 	err := tmpl.Execute(&buf, d)
